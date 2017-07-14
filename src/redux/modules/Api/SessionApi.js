faffd54b32dedcb60aa369e2fe1fb90f2f4b8258
Michael Ries <michael.ries@gmail.com>
2017-07-13 19:16:11 -0500

    add SessionApi.js

---
+++ src/redux/modules/Api/SessionApi.js
@@ -0,0 +1,20 @@
+class SessionApi {
+  static login(credentials) {
+    const request = new Request(`${process.env.API_HOST}/login`, {
+      method: 'POST',
+      headers: new Headers({
+        'Content-Type': 'application/json'
+      }), 
+      body: JSON.stringify({auth: credentials})
+    });
+
+
+    return fetch(request).then(response => {
+      return response.json();
+    }).catch(error => {
+      return error;
+    });
+  } 
+}
+
+export default SessionApi;
