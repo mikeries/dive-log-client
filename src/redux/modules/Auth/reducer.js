4de8581d2a5e8cc805dccbde5b07e87e6a81f75a
Michael Ries <michael.ries@gmail.com>
2017-07-15 14:35:36 -0500

    refactor

--- src/redux/modules/Auth/reducer.js
+++ src/redux/modules/Auth/reducer.js
@@ -1,9 +1,6 @@
 import * as actions from './actionTypes' 
 
 const initialState = {
-  isAuthenticated: false,
-  isAuthenticating: true,
-  currentUser: {},
   token: null,
   errors: []
 }
@@ -11,7 +8,9 @@ const initialState = {
 export default function sessionReducer(state = initialState, action) {  
   switch(action.type) {
     case actions.LOG_IN:
-      return !!sessionStorage.jwt
+      return state; // for now...
+    case actions.LOG_OUT:
+      return state;
     default: 
       return state;
   }
--- src/redux/modules/Auth/sessionActions.js
+++ src/redux/modules/Auth/sessionActions.js
@@ -1,13 +1,11 @@
-
 import * as actions from './actionTypes';
-import sessionApi from '../Api/SessionApi';
 
 export function loginUser(token) {
   sessionStorage.setItem('jwt', token);
-  return {type: actions.LOG_IN_USER}
+  return {type: actions.LOG_IN}
 }
 
 export function logOutUser() {
   sessionStorage.removeItem('jwt');
-  return {type: actions.LOG_OUT_USER}
+  return {type: actions.LOG_OUT}
 }
