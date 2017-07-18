bb85588132ba049b5c5b58008ca368b02934bdff
Michael Ries <michael.ries@gmail.com>
2017-07-18 14:34:12 -0500

    code cleanup

--- src/views/diveShow.js
+++ src/views/diveShow.js
@@ -4,12 +4,15 @@ import { Link } from 'react-router-dom';
 const DiveShow = ({ dive }) => (
   <div>
     <h3>{dive.datetime} {dive.location.name} - {dive.location.city}, {dive.location.country}</h3>
+    
     <p>Duration: {dive.duration}</p>
     <p>Ballast: {dive.ballast}  Maximum Depth: {dive.max_depth}</p>
     <p>Starting Pressure: {dive.starting_pressure}  Final Pressure: {dive.final_pressure}</p>
+    
     <h4>Comments</h4>
     <p>{dive.comments}</p>
     <br/>
+    
     <Link to={`/dives/${dive.id}/edit`}>Edit</Link>
   </div>
 )
