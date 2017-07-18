0081790da4e2d78f8681a589950ad28e3a53b2e0
Michael Ries <michael.ries@gmail.com>
2017-07-18 14:09:03 -0500

    refactor into presentaional component

--- src/views/components/LoginButton.js
+++ src/views/components/LoginButton.js
@@ -1,22 +1,16 @@
-
-import React, { Component } from 'react';
+import React from 'react';
 import Button from 'react-toolbox/lib/button/Button';
 
-class LoginButton extends Component {
-  render() {
-    console.log(this.props)
-    return (
-      <div>
-        <Button
-          raised
-          accent
-          href={`http://localhost:3000/authenticate?type=${this.props.type}`}
-        >
-          Login with {this.props.type}
-        </Button>
-      </div>
-    );
-  }
-}
+const LoginButton = (props) => (
+  <div>
+    <Button
+      raised
+      accent
+      href={`http://localhost:3000/authenticate?type=${props.type}`}
+    >
+      Login with {props.type}
+    </Button>
+  </div>
+)
 
 export default LoginButton;
