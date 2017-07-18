dfb6307a0c5057becc168fd1ab6d657329c118c1
Michael Ries <michael.ries@gmail.com>
2017-07-18 16:01:14 -0500

    add 'new' link to bottom of list

--- src/views/dives/List.js
+++ src/views/dives/List.js
@@ -2,7 +2,10 @@ import React from 'react';
 import { Link } from 'react-router-dom'
 import Dive from './Dive'
 
-const DiveList = ({dives}) => {
+const DiveList = ({
+  dives,
+  handleNewClick
+}) => {
   const renderDives = dives.map((dive) => (
     <Link key={dive.id} to={`/dives/${dive.id}`}>
       <Dive key={dive.id} dive={dive}/>
@@ -13,6 +16,7 @@ const DiveList = ({dives}) => {
     <div>
       <h1>Your Dives</h1>
       {renderDives}
+      <Link to={`/dives/new`}>New</Link>
     </div>
   );
 }
