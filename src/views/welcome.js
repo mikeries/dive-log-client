4322542c641aca3d14b73c77428643fa5c8913c4
Michael Ries <michael.ries@gmail.com>
2017-07-18 14:36:26 -0500

    tidying code

--- src/views/welcome.js
+++ src/views/welcome.js
@@ -1,25 +1,21 @@
-import React, { Component } from 'react';
+import React from 'react';
 import LoginButton from './components/LoginButton';
 
-class Welcome extends Component {
-  render() {
-    return (
-      <div className="Login">
-        <div className="Login-header">
-          <h2 className="Login-title">
-            Dive Log
-          </h2>
-          <p className="Login-intro">
-            Cloud-based Dive Log
-          </p>
-        </div>
-        <div className="Login-buttons">
-          <LoginButton type='facebook'/>
-            <br/>
-          <LoginButton type='github'/>
-        </div>
-      </div>
-    );
-  }
-}
+const Welcome = (props) => (
+  <div className="Login">
+    <div className="Login-header">
+      <h2 className="Login-title">
+        Dive Log
+      </h2>
+      <p className="Login-intro">
+        Cloud-based Dive Log
+      </p>
+    </div>
+    <div className="Login-buttons">
+      <LoginButton type='facebook'/>
+        <br/>
+      <LoginButton type='github'/>
+    </div>
+  </div>
+)
 export default Welcome
