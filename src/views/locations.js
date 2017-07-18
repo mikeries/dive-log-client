875eb2215af82729d56c3570b55db330668c870a
Michael Ries <michael.ries@gmail.com>
2017-07-18 14:05:35 -0500

    refactor locations into presentational component

--- src/views/locations.js
+++ src/views/locations.js
@@ -1,23 +1,19 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
 import Location from './components/Location'
 
-class Locations extends Component {
-  render() {
-    return (
-      <div>
-        <h1>Locations Page</h1>
-        {this.props.locations && this.props.locations.map(
-          (location, index) => ( <Location key={index} location={location} />)
-        )}
-      </div>
-    );
-  }
-}
+const Locations = (props) => (
+  <div>
+    <h1>Locations Page</h1>
+    {props.locations && props.locations.map(
+      (location, index) => ( <Location key={index} location={location} />)
+    )}
+  </div>
+)
+
 
 const mapStateToProps = (state) => {
   return { 
-    jwt: state.sessionReducer.jwt,
     locations: state.locationsReducer.locations
   };
 }
