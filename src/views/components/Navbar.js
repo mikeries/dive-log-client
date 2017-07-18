738a694a8a4b3c60fc45cd2582f8ad4dbf92df0b
Michael Ries <michael.ries@gmail.com>
2017-07-18 14:26:56 -0500

    refactor navbar and remove inline styling

--- src/views/components/Navbar.js
+++ src/views/components/Navbar.js
@@ -1,43 +1,21 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
-class NavBar extends React.Component {
-
-  handleLogout = () => {
-    this.props.handleLogout(this.context.router);
-  }
-
-  render() {
-    return (
-      <div style={{ borderBottom: '2px solid black', paddingBottom: '10px', marginBottom: '12px' }}>
-        <NavLink 
-          style={{ marginRight: '10px' }} 
-          to="/"
-        >
-          Home
-        </NavLink>
-        <NavLink 
-          style={{ marginRight: '10px' }} 
-          to="/dives"
-        >
-          Dives
-        </NavLink>
-        <NavLink 
-          style={{ marginRight: '10px' }} 
-          to="/locations"
-        >
-          Locations
-        </NavLink>
-        <NavLink 
-          style={{ marginRight: '10px' }} 
-          to="/"
-          onClick={this.props.handleLogout}
-        >
-          Logout
-        </NavLink>
-      </div>
-    );
-  }
-}
+const NavBar = (props) => (
+  <div className='navbar'>
+    <NavLink to="/">
+      Home
+    </NavLink>
+    <NavLink to="/dives">
+      Dives
+    </NavLink>
+    <NavLink to="/locations">
+      Locations
+    </NavLink>
+    <NavLink to="/" onClick={props.handleLogout}>
+      Logout
+    </NavLink>
+  </div>
+)
  
 export default NavBar;
