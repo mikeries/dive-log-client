a45f35d5ae92d830e313af9b5d56fb20ae4d9db1
Michael Ries <michael.ries@gmail.com>
2017-07-18 22:24:39 -0500

    add styling to form

--- src/views/dives/Form.js
+++ src/views/dives/Form.js
@@ -1,5 +1,12 @@
 import React, { Component } from 'react';
-import { Button } from 'react-bootstrap';
+import { 
+  Button,
+  Form,
+  FormGroup,
+  ControlLabel,
+  FormControl
+ } from 'react-bootstrap';
+
 
 class DiveForm extends Component  {
 
@@ -34,46 +41,78 @@ class DiveForm extends Component  {
 
   render() {
     return (
-    <form onSubmit={this.handleFormSubmit}>
-      <h1>Editing the dive</h1>
-      <label>Date: <input type='text' 
-                  value={this.state.datetime}
-                  name='datetime' 
-                  onChange={this.handleInputChange} /></label>
-      <label>Location: 
-        <select value={this.state.location_id} name='location' onChange={this.handleLocationChange}>
+    <Form onSubmit={this.handleFormSubmit}>
+      {this.props.dive.id==0 ? <h1>Editing dive</h1> : <h1>New Dive</h1>}
+
+      <FormGroup>
+        <ControlLabel>Date</ControlLabel>
+        <FormControl
+          type="text"
+          value={this.state.value}
+          placeholder="Date of dive"
+          name='datetime'
+          onChange={this.handleInputChange}
+        />
+      </FormGroup>
+
+      <FormGroup>
+        <ControlLabel>Location</ControlLabel>
+        <FormControl componentClass='select'
+          value={this.state.location_id} name='location' onChange={this.handleLocationChange}>
           {this.props.locations && this.props.locations.map(location => (
             <option value={location.id} key={location.id}>{location.name}</option>)
           )}
-        </select>
-      </label>
+        </FormControl>
+      </FormGroup>
       <br/>
-      <br/>
-      <label>Ballast: <input type='text' 
-        value={this.state.ballast} 
-        name='ballast' 
-        onChange={this.handleInputChange} />
-      </label>
-      <label>Maximum Depth: <input type='text' 
-        value={this.state.max_depth} 
-        name='max_depth' 
-        onChange={this.handleInputChange} />
-      </label>
-      <label>Starting Pressure: <input type='text' 
-        value={this.state.starting_pressure} 
-        name='starting_pressure' 
-        onChange={this.handleInputChange} />
-      </label>
-      <label>Final Pressure: <input type='text' 
-        value={this.state.final_pressure} 
-        name='final_pressure' 
-        onChange={this.handleInputChange} />
-      </label>
-      <h4>Comments</h4>
-      <textarea value={this.state.comments} name='comments' onChange={this.handleInputChange}></textarea>
+
+      <FormGroup>
+        <ControlLabel>Ballast</ControlLabel>
+        <FormControl 
+          type='text' 
+          value={this.state.ballast} 
+          name='ballast' 
+          onChange={this.handleInputChange} />
+      </FormGroup>
+
+      <FormGroup>
+        <ControlLabel>Maximum Depth</ControlLabel>
+        <FormControl 
+          type='text' 
+          value={this.state.max_depth} 
+          name='max_depth' 
+          onChange={this.handleInputChange} />
+      </FormGroup>
+
+      <FormGroup>
+        <ControlLabel>Starting Pressure</ControlLabel>
+        <FormControl 
+          type='text' 
+          value={this.state.starting_pressure} 
+          name='starting_pressure' 
+          onChange={this.handleInputChange} />
+      </FormGroup>
+
+      <FormGroup>
+        <ControlLabel>Final Pressure</ControlLabel>
+        <FormControl 
+          type='text' 
+          value={this.state.final_pressure} 
+          name='final_pressure' 
+          onChange={this.handleInputChange} />
+      </FormGroup>
+
+      <FormGroup>
+        <ControlLabel>Comments</ControlLabel>
+        <FormControl componentClass='textarea'
+          value={this.state.comments} 
+          name='comments' 
+          onChange={this.handleInputChange} />
+      </FormGroup>
+
       <br/>
       <Button type='submit'>Save</Button>
-    </form>
+    </Form>
     );
   }
 }
