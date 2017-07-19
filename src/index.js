ceffadc2a08833e2592c0ee2fa0d4cda01deb16f
Michael Ries <michael.ries@gmail.com>
2017-07-19 08:59:05 -0500

    install bootstrap styles locally

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import store from './redux/store';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap-theme.css';
 import './index.css';
 import App from './containers/App';
 import registerServiceWorker from './registerServiceWorker';
