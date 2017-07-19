94f26e02555fb38719dda60c153e798775bb4098
Michael Ries <michael.ries@gmail.com>
2017-07-18 19:26:50 -0500

    remove react-toolbox and react-toolbox-themr

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import ThemeProvider from 'react-toolbox/lib/ThemeProvider';
-import theme from './toolbox/theme';
 import store from './redux/store';
 import './index.css';
 import App from './containers/App';
@@ -10,9 +8,7 @@ import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
   <Provider store={store}>
-    <ThemeProvider theme={theme}>
       <App />
-    </ThemeProvider>
   </Provider>,
   document.getElementById('root')
 );
--- src/views/components/LoginButton.js
+++ src/views/components/LoginButton.js
@@ -1,15 +1,10 @@
 import React from 'react';
-import Button from 'react-toolbox/lib/button/Button';
 
 const LoginButton = (props) => (
   <div>
-    <Button
-      raised
-      accent
-      href={`http://localhost:3000/authenticate?type=${props.type}`}
-    >
+    <a href={`http://localhost:3000/authenticate?type=${props.type}`}>
       Login with {props.type}
-    </Button>
+    </a>
   </div>
 )
 
