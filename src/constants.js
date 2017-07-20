f398843a52d79214b5b7cf4d391b2187170ca368
Michael Ries <michael.ries@gmail.com>
2017-07-20 11:39:02 -0500

    create static.json

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,2 @@
 export const API_URL = 'https://powerful-reef-50818.herokuapp.com';
-export const CLIENT_URL = 'https://dive-log-client.herokuapp.com/';
+export const CLIENT_URL = 'https://dive-log-client.herokuapp.com';
