44ae549f5b3529b0a393215613babc28f195f86c
Michael Ries <michael.ries@gmail.com>
2017-07-20 13:11:54 -0500

    add stats back in with additional checks for 0 dives

--- src/views/dashboard.js
+++ src/views/dashboard.js
@@ -6,7 +6,7 @@ const totalDuration = dives => (
 )
 
 const deepestDive = dives => {
-  if (dives) {
+  if (dives.length > 0) {
     return dives.reduce((deepest,dive) => (
       dive = deepest.max_depth < dive.max_depth ? dive : deepest
     ), dives[0])
@@ -17,7 +17,33 @@ const Dashboard = ({user, dives}) => {
   const deepest = deepestDive(dives);
   return (
     <Grid>
+      {dives.length > 0 &&
+        <div>
+          <h2>Your dive statistics</h2>
+ 
+          <div>
+            <h3>Number of dives</h3>
+            <p>{dives.length}</p>
+          </div>
 
+          <div>
+            <h3>Total duration</h3>
+            <p>{totalDuration(dives)} minutes</p>
+          </div>
+
+          deepest && <div>
+            <h3>Deepest dive</h3>
+            <p>{deepest.location.name} on {deepest.date}</p>
+          </div>
+          
+
+          <div>
+            <h3>Maximum depth</h3>
+            <p>{deepest.max_depth} feet</p>
+          </div>
+
+        </div>
+      }
     </Grid>
   )
 }
