2b79a64841a509d894b16a945e8acc1eeb0e2e55
Michael Ries <michael.ries@gmail.com>
2017-07-21 10:19:00 -0500

    update API url for production

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,3 @@
-export const API_URL = 'http://localhost:3000';
+//export const API_URL = 'http://localhost:3000';
+export const API_URL = 'https://powerful-reef-50818.herokuapp.com/'
 export const NOOP = () => {}
