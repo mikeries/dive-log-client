27bfa2987839a9165beec31c044fc208e9139d13
Michael Ries <michael.ries@gmail.com>
2017-08-04 14:29:36 -0500

    remove service working import

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap-theme.css';
 import './index.css';
 import App from './containers/App';
-import registerServiceWorker from './registerServiceWorker';
+//import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
   <Provider store={store}>
