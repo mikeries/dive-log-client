a773b6f46193a258e9d94f71c08a360dae6dfc01
Michael Ries <michael.ries@gmail.com>
2017-08-07 14:00:00 -0500

    create locations/Form

--- src/views/locations/Form.js
+++ src/views/locations/Form.js
@@ -12,28 +12,19 @@ import {
   Well
  } from 'react-bootstrap';
 
-import {DIVES_ROOT } from '../../constants';
+import { LOCATIONS_ROOT } from '../../constants';
  import ErrorList from '../components/ErrorList';
 
-class DiveForm extends Component  {
+class LocationForm extends Component  {
 
   componentWillMount() {
     this.state = {
-      ...this.props.dive,
-      locations: this.props.locations
+      ...this.props.location
     };
   }
 
   componentWillReceiveProps() {
     this.setState({ submitted: false });
-  }
-
-  handleLocationChange = event => {
-    event.preventDefault();
-    this.setState({
-      location_id: event.target.value,
-      location: this.props.locations.find(location => location.id === +event.target.value)
-    });
   }
 
   handleInputChange = event => {
@@ -56,7 +47,7 @@ class DiveForm extends Component  {
     return (
       <Grid>
         <Form onSubmit={this.handleFormSubmit}>
-          {this.state.id > 0 ? <h1>Editing Dive</h1> : <h1>New Dive</h1>}
+          {this.state.id > 0 ? <h1>Editing Location</h1> : <h1>New Location</h1>}
 
           {this.props.errors &&
             <Row>
@@ -65,105 +56,55 @@ class DiveForm extends Component  {
               </Col>
             </Row>
           }
-
           <Row>
             <Col md={3}>
               <FormGroup>
-                <ControlLabel>Date</ControlLabel>
+                <ControlLabel>Name</ControlLabel>
                 <FormControl
                   type="text"
-                  value={this.state.date}
-                  placeholder="Date of dive"
-                  name='date'
+                  value={this.state.name}
+                  placeholder="location name"
+                  name='name'
+                  onChange={this.handleInputChange}
+                />
+              </FormGroup>
+            </Col>
+
+            <Col md={3}>
+              <FormGroup>
+                <ControlLabel>City</ControlLabel>
+                <FormControl
+                  type="text"
+                  value={this.state.city}
+                  placeholder="location city"
+                  name='city'
+                  onChange={this.handleInputChange}
+                />
+              </FormGroup>
+            </Col>
+
+            <Col md={3}>
+              <FormGroup>
+                <ControlLabel>Country</ControlLabel>
+                <FormControl
+                  type="text"
+                  value={this.state.country}
+                  placeholder="location country"
+                  name='country'
                   onChange={this.handleInputChange}
                 />
               </FormGroup>
             </Col>
             <Col md={3}>
               <FormGroup>
-                <ControlLabel>Time</ControlLabel>
+                <ControlLabel>Category</ControlLabel>
                 <FormControl
                   type="text"
-                  value={this.state.time}
-                  placeholder="Time of dive"
-                  name='time'
+                  value={this.state.category}
+                  placeholder="type of dive (wall, reef, wreck, etc)"
+                  name='category'
                   onChange={this.handleInputChange}
                 />
-              </FormGroup>
-            </Col>
-            <Col md={3}>
-              <FormGroup>
-                <ControlLabel>Location</ControlLabel>
-                <FormControl componentClass='select'
-                  value={this.state.location_id} 
-                  name='location' 
-                  onChange={this.handleLocationChange}>
-                    {this.props.locations && this.props.locations.map(location => (
-                      <option value={location.id} key={location.id}>{location.name}</option>)
-                    )}
-                </FormControl>
-              </FormGroup>
-            </Col>
-            <Col md={3}>
-              <FormGroup>
-                <ControlLabel>Duration</ControlLabel>
-                <FormControl
-                  type="text"
-                  value={this.state.duration}
-                  placeholder="Duration of dive, in minutes"
-                  name='duration'
-                  onChange={this.handleInputChange}
-                />
-              </FormGroup>
-            </Col>
-          </Row>
-
-          <Row>
-            <Col md={3}>
-              <FormGroup>
-                <ControlLabel>Ballast</ControlLabel>
-                <FormControl 
-                  type='text' 
-                  value={this.state.ballast} 
-                  placeholder='How many pounds of ballast'
-                  name='ballast' 
-                  onChange={this.handleInputChange} />
-              </FormGroup>
-            </Col>
-
-            <Col md={3}>
-              <FormGroup>
-                <ControlLabel>Maximum Depth</ControlLabel>
-                <FormControl 
-                  type='text' 
-                  value={this.state.max_depth}
-                  placeholder='Maximum depth, in feet'
-                  name='max_depth' 
-                  onChange={this.handleInputChange} />
-              </FormGroup>
-            </Col>
-
-            <Col md={3}>
-              <FormGroup>
-                <ControlLabel>Starting Pressure</ControlLabel>
-                <FormControl 
-                  type='text' 
-                  value={this.state.starting_pressure}
-                  placeholder='Initial pressure, in psi'
-                  name='starting_pressure' 
-                  onChange={this.handleInputChange} />
-              </FormGroup>
-            </Col>
-
-            <Col md={3}>
-              <FormGroup>
-                <ControlLabel>Final Pressure</ControlLabel>
-                <FormControl 
-                  type='text' 
-                  value={this.state.final_pressure}
-                  placeholder='Final pressure, in psi'
-                  name='final_pressure' 
-                  onChange={this.handleInputChange} />
               </FormGroup>
             </Col>
           </Row>
@@ -171,16 +112,16 @@ class DiveForm extends Component  {
           <Row>
             <Col md={12}>
               <FormGroup>
-                <ControlLabel>Comments</ControlLabel>
+                <ControlLabel>Description</ControlLabel>
                 <FormControl componentClass='textarea'
-                  value={this.state.comments} 
-                  name='comments' 
+                  value={this.state.description} 
+                  name='description' 
                   onChange={this.handleInputChange} />
               </FormGroup>
             </Col>
           </Row>
 
-          <Link to={`${DIVES_ROOT}`}>
+          <Link to={`${LOCATIONS_ROOT}`}>
             <Button>Back</Button>
           </Link>
           <Button disabled={!!this.state.submitted} type='submit'>Save</Button>
@@ -190,4 +131,4 @@ class DiveForm extends Component  {
   }
 }
 
-export default DiveForm
+export default LocationForm
