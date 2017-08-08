be5f34b7e92cf1d893c0ad2f831f34793ff547c3
Michael Ries <michael.ries@gmail.com>
2017-08-08 17:01:31 -0500

    gaurd against null dive

--- src/views/dives/Show.js
+++ src/views/dives/Show.js
@@ -7,48 +7,52 @@ import { DIVES_ROOT } from '../../constants';
 const DiveShow = ({ 
   dive,
   onDelete
-}) => (
-  <Grid>
-    <Row>
-      <Col md={12}>
-        <h3>{dive.location.name} - {dive.location.city}, {dive.location.country}</h3>
-      </Col>
-    </Row>
+}) => {
+  if (dive && dive.location) {
+    return (
+      <Grid>
+        <Row>
+          <Col md={12}>
+            <h3>{dive.location.name} - {dive.location.city}, {dive.location.country}</h3>
+          </Col>
+        </Row>
 
-    <Row>
-      <Col md={12}>
-        <p>on {dive.date} at {dive.time}</p>
-      </Col>
-    </Row>
-    <br/>
+        <Row>
+          <Col md={12}>
+            <p>on {dive.date} at {dive.time}</p>
+          </Col>
+        </Row>
+        <br/>
 
-    <Row>
-      <Col md={2} mdOffset={1}><p>Duration: {dive.duration}</p></Col>
-      <Col md={2}><p>Ballast: {dive.ballast}</p></Col>
-      <Col md={2}><p>Maximum Depth: {dive.max_depth}</p></Col>
-      <Col md={2}><p>Starting Pressure: {dive.starting_pressure}</p></Col>
-      <Col md={2}><p>Final Pressure: {dive.final_pressure}</p></Col>
-    </Row>
-    <br/>
+        <Row>
+          <Col md={2} mdOffset={1}><p>Duration: {dive.duration}</p></Col>
+          <Col md={2}><p>Ballast: {dive.ballast}</p></Col>
+          <Col md={2}><p>Maximum Depth: {dive.max_depth}</p></Col>
+          <Col md={2}><p>Starting Pressure: {dive.starting_pressure}</p></Col>
+          <Col md={2}><p>Final Pressure: {dive.final_pressure}</p></Col>
+        </Row>
+        <br/>
 
-    {dive.comments &&
-      <Row>
-        <Col md={12}>
-          <h4>Comments</h4>
-          <p>{dive.comments}</p>
-        </Col>
-      </Row>
-    }
-    <br/>
-    
-    <Link to={`${DIVES_ROOT}`}>
-      <Button>Back</Button>
-    </Link>
-    <Link to={`${DIVES_ROOT}/${dive.id}/edit`}>
-      <Button>Edit</Button>
-    </Link>
-    <Button onClick={() => onDelete(dive.id)}>Delete</Button>
-  </Grid>
-)
+        {dive.comments &&
+          <Row>
+            <Col md={12}>
+              <h4>Comments</h4>
+              <p>{dive.comments}</p>
+            </Col>
+          </Row>
+        }
+        <br/>
+        
+        <Link to={`${DIVES_ROOT}`}>
+          <Button>Back</Button>
+        </Link>
+        <Link to={`${DIVES_ROOT}/${dive.id}/edit`}>
+          <Button>Edit</Button>
+        </Link>
+        <Button onClick={() => onDelete(dive.id)}>Delete</Button>
+      </Grid>
+    )
+  } else return (<div></div>)
+}
 
 export default DiveShow
