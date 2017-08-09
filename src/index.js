fdee1f9f86eb7e1769290ef78e0e9b4adb0cb646
Michael Ries <michael.ries@gmail.com>
2017-08-09 11:20:27 -0500

    remove bootstrap imports

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import store from './redux/store';
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap/dist/css/slate-theme.min.css';
 import './index.css';
 import App from './containers/App';
 //import registerServiceWorker from './registerServiceWorker';
