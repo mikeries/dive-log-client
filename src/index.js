70934126fca07f9ef84faf8ca5dc64f2f5ad16b7
Michael Ries <michael.ries@gmail.com>
2017-08-09 14:47:41 -0500

    modify slate theme and put it in src

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import store from './redux/store';
 import 'bootstrap/dist/css/bootstrap.css';
-import './slate-theme.min.css';
-import './react-datepicker.css'
+import './bootstrap-slate-theme.css';
+import './react-datepicker.css';
 import './index.css';
 import App from './containers/App';
 //import registerServiceWorker from './registerServiceWorker';
