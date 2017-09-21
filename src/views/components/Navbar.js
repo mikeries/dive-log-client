77499b979e985bc8c705fb8410722f530bc725c3
Michael Ries <michael.ries@gmail.com>
2017-09-21 09:54:31 -0500

    re-structure navbar

--- src/views/components/Navbar.js
+++ src/views/components/Navbar.js
@@ -1,24 +1,34 @@
 import React from 'react';
 import { Navbar, NavItem, Nav } from 'react-bootstrap';
+import { Link } from 'react-router-dom'
 import { LinkContainer } from 'react-router-bootstrap';
 
 const NavBar = ({ user, handleLogout }) => (
-  <Navbar>
-    <Nav>
-      <LinkContainer to='/dashboard'><NavItem>Dive Log Dashboard</NavItem></LinkContainer>
-      <LinkContainer to='/dives'><NavItem>Dives</NavItem></LinkContainer>
-      <LinkContainer to='/locations'><NavItem>Locations</NavItem></LinkContainer>
-      <LinkContainer to='/about'><NavItem>About</NavItem></LinkContainer>
-    </Nav>
+  <div className="container">
+  <Navbar fluid collapseOnSelect>
+    <Navbar.Header>
+      <Navbar.Brand>
+        <Link to='/dashboard'>Dive Log Dashboard</Link>
+      </Navbar.Brand>
+    </Navbar.Header>
 
-    <Nav right>
-      {user && user.name && <NavItem id='username' to=''>Welcome {user.name}!</NavItem>}
-      <LinkContainer to='/logout' >
-        <NavItem onClick={handleLogout}>Logout</NavItem>
-      </LinkContainer>
-    </Nav>
+    <Navbar.Collapse>
+      <Nav>
+        <LinkContainer to='/dives'><NavItem>Dives</NavItem></LinkContainer>
+        <LinkContainer to='/locations'><NavItem>Locations</NavItem></LinkContainer>
+        <LinkContainer to='/about'><NavItem>About</NavItem></LinkContainer>
+      </Nav>
+      
+      <Nav pullRight>
+        {user && user.name && <NavItem id='username' to=''>Welcome {user.name}!</NavItem>}
+        <LinkContainer to='/logout' >
+          <NavItem onClick={handleLogout}>Logout</NavItem>
+        </LinkContainer>
+      </Nav>
+    </Navbar.Collapse>
     
   </Navbar>
+  </div>
 );
  
 export default NavBar;
