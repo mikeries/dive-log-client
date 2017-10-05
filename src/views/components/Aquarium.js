0ccc276735b963db495032043e3adcf37c3d2082
Michael Ries <michael.ries@gmail.com>
2017-10-05 11:37:00 -0500

    set .water height/width to 100vh/vw to automatically resize with window

--- src/views/components/Aquarium.js
+++ src/views/components/Aquarium.js
@@ -3,16 +3,16 @@ import Fish from './Fish'
 
 class Aquarium extends Component {
   render() {
-    let waterStyle = {height: window.innerHeight, width: window.innerWidth}
+    let waterStyle = {height: '100vh', width: '100vw'}
     return (
       <div id='aquarium'>
-        <div className='water' style={{...waterStyle, zIndex:-1}}></div>
-        <div className='water' style={{...waterStyle, zIndex:-10}}></div>
-        <div className='water' style={{...waterStyle, zIndex:-20}}></div>
-        <div className='water' style={{...waterStyle, zIndex:-30}}></div>
-        <div className='water' style={{...waterStyle, zIndex:-40}}></div>
-        <div className='water' style={{...waterStyle, zIndex:-50}}></div>
-        <div className='water' style={{...waterStyle, zIndex:-60}}></div>
+        <div className='water' style={{zIndex: -1}}></div>
+        <div className='water' style={{zIndex: -10}}></div>
+        <div className='water' style={{zIndex: -20}}></div>
+        <div className='water' style={{zIndex: -30}}></div>
+        <div className='water' style={{zIndex: -40}}></div>
+        <div className='water' style={{zIndex: -50}}></div>
+        <div className='water' style={{zIndex: -60}}></div>
         <Fish image='./images/tiny-small-pixel-fish-aquarium-animated-gif-picture-10.gif'/>
         <Fish image='./images/tiny-small-pixel-fish-aquarium-animated-gif-picture-19.gif'/>
         <Fish image='./images/tiny-small-pixel-fish-aquarium-animated-gif-picture-11.gif'/>
